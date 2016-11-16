b5f8e432ba4782944917a6be4085696896456899
hsh97Erica <imscs21@hanyang.ac.kr>
2016-11-16 14:33:03 +0900

    maniplate library interface file

--- app/src/main/java/com/trippleit/android/tetris3d/shapes/IShape.java
+++ app/src/main/java/com/trippleit/android/tetris3d/shapes/IShape.java
@@ -1,10 +1,12 @@
 package com.trippleit.android.tetris3d.shapes;
 
 import javax.microedition.khronos.opengles.GL10;
-
+import org.sms.tetris3d.players.*;
 public interface IShape {
-	
+	public User getUser();
+	public void setUser(User usr);
 	public void draw(GL10 gl);
+	public void drawLineBone(GL10 gl);
 	public boolean[][][] getObjectMatrix();
 	public String getColor();
 	public int getXsize();
