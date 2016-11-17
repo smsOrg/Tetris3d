9d4be0d28fd614f0252a60271e80c386350371ee
hsh97Erica <imscs21@hanyang.ac.kr>
2016-11-17 11:13:02 +0900

    add nextblock renderer

---
+++ app/src/main/java/org/sms/tetris3d/render/NextBlockRenderer.java
@@ -0,0 +1,15 @@
+package org.sms.tetris3d.render;
+
+
+import javax.microedition.khronos.opengles.GL10;
+
+/**
+ * Created by hsh on 2016. 11. 17..
+ */
+
+public class NextBlockRenderer extends com.trippleit.android.tetris3d.render.AbstractOpenGlRenderer {
+    @Override
+    public void onDrawFrame(GL10 gl, boolean firstDraw) {
+
+    }
+}
