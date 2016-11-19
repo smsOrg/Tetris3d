63d8a15e6dae26ff8a42118a7c108f5d310c7e92
hsh97Erica <imscs21@hanyang.ac.kr>
2016-11-19 18:37:11 +0900

    add gameactivity

--- app/src/main/java/org/sms/tetris3d/GameStatus.java
+++ app/src/main/java/org/sms/tetris3d/GameStatus.java
@@ -14,47 +14,21 @@ import org.sms.tetris3d.players.User;
  */
 
 public class GameStatus extends com.trippleit.android.tetris3d.GameStatus{
-    protected static final  ArrayList<User> players = new ArrayList<User>(){
-        @Override
-        public User remove(int idx){
-            if(idx>0){
-                return super.remove(idx);
-            }
-            else{
-                return get(0);
-            }
-        }
-        @Override
-        public void clear(){
-            final User usr = get(0);
-            super.clear();
-            add(usr);
-        }
-        @Override
-        public boolean remove(Object usr){
-            if(usr instanceof  DeviceUser){
-                return false;
-            }else{
-                return super.remove(usr);
-            }
-        }
-        @Override
-        public void removeRange(int start,int end){
-            super.removeRange(start>0?start:1,end>0?end:1);
-        }
+    protected static final  RegisteredPlayers players = new RegisteredPlayers(){
+
     };
     public static ArrayList<User> getPlayers(){
         return players;
     }
-    public static int getAvailableZPos(final org.sms.tetris3d.players.User who){
+    public static int getAvailableZPos(final User who){
         int result=0;
         for (int i = 0; i <= who.getCurrentObjectZ(); i++) {
             boolean isExist = false;
             for(int j =0;!isExist&&j<who.getCurrentObject().getObjectMatrix().length;j++){
                 for(int k =0;!isExist&&k<who.getCurrentObject().getObjectMatrix()[j].length;k++) {
                     for (int l = 0;!isExist&&l<who.getCurrentObject().getObjectMatrix()[j][k].length;l++) {
-                        final boolean isValid = (j + who.getCurrentObjectX()) < com.trippleit.android.tetris3d.GameStatus.getGameBoolMatrix().length && (k + who.getCurrentObjectY()) < com.trippleit.android.tetris3d.GameStatus.getGameBoolMatrix()[0].length&&(l + i) < com.trippleit.android.tetris3d.GameStatus.getGameBoolMatrix()[0][0].length;
-                        if (who.getCurrentObject().getObjectMatrix()[j][k][l] && isValid && com.trippleit.android.tetris3d.GameStatus.getGameBoolMatrix()[j + who.getCurrentObjectX()][k + who.getCurrentObjectY()][i+l]) {
+                        final boolean isValid = (j + who.getCurrentObjectX()) < GameStatus.getGameBoolMatrix().length && (k + who.getCurrentObjectY()) < GameStatus.getGameBoolMatrix()[0].length&&(l + i) < GameStatus.getGameBoolMatrix()[0][0].length;
+                        if (who.getCurrentObject().getObjectMatrix()[j][k][l] && isValid && GameStatus.getGameBoolMatrix()[j + who.getCurrentObjectX()][k + who.getCurrentObjectY()][i+l]) {
                             isExist = true;
                         }
                     }
@@ -67,12 +41,13 @@ public class GameStatus extends com.trippleit.android.tetris3d.GameStatus{
         return result;
     }
     public static void init(Context _c) {
+        players.forceClear();
         players.add(new DeviceUser());
-        gameHeight = 10;
-        gridSize = 5;
+        gameHeight = 12;
+        gridSize = 6;
         gStatus = GAME_STATUS.START;
         restartGameBoolMatrix();
-        setCamera(-65, 10);
+        setCamera(initialCameraAngle, gameHeight);
         start=true;
         end = false;
         startX = 2;
---
+++ app/src/main/java/org/sms/tetris3d/MainGameActivity.java
@@ -0,0 +1,119 @@
+package org.sms.tetris3d;
+
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.widget.*;
+import android.opengl.*;
+
+import org.sms.tetris3d.players.Computer;
+import org.sms.tetris3d.render.*;
+import org.sms.tetris3d.interfaces.*;
+import org.sms.tetris3d.controls.*;
+import com.trippleit.android.tetris3d.controls.*;
+/**
+ * Created by hsh on 2016. 11. 19..
+ */
+
+public class MainGameActivity extends Activity {
+    private boolean timerLoopAvailable = true;
+    public void onCreate(Bundle onSavedInstanceState){
+        super.onCreate(onSavedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_main);
+
+        GLSurfaceView glView = (GLSurfaceView) findViewById(R.id.glSurface);
+        GameRenderer renderer = new GameRenderer();
+        glView.setRenderer(renderer);
+        GLSurfaceView glView2 = (GLSurfaceView) findViewById(R.id.glSurface2);
+        NextBlockRenderer renderer2 = new NextBlockRenderer();
+        LinearLayout llt = (LinearLayout)findViewById(R.id.mainviewcontrolpanel);
+        llt.getLayoutParams().height = (int)(getResources().getDisplayMetrics().heightPixels*0.108);
+        Button pause_btn = (Button)findViewById(R.id.btn_pause);
+        pause_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(GameStatus.getGameStatus()== GameStatus.GAME_STATUS.PAUSE){
+                    GameStatus.setGameStatus(GameStatus.GAME_STATUS.ONGOING);
+                }else
+                    GameStatus.setGameStatus(GameStatus.GAME_STATUS.PAUSE);
+            }
+        });
+        llt.setLayoutParams(llt.getLayoutParams());
+        glView2.getLayoutParams().width = llt.getLayoutParams().height;
+        glView2.setLayoutParams(glView2.getLayoutParams());
+        glView2.setRenderer(renderer2);
+
+        glView.requestFocus();
+        glView.setFocusableInTouchMode(true);
+        final TextView lncnt_view = (TextView)findViewById(R.id.line_count_view);
+        GameStatus.setOnRemoveOneLayer(new OnRemoveLayer() {
+            @Override
+            public void onRemove(final long rem_count) {
+                runOnUiThread(new Runnable(){
+                    public void run(){
+                        lncnt_view.setText(rem_count+"");
+                    }
+                });
+            }
+        });
+
+        GameStatus.init(this);
+       // GameStatus.getPlayers().add(new Computer());
+        glView.setOnTouchListener(new SwipeControls(this));
+        glView2.setOnClickListener(new ChangeBlockControls());
+
+        Button b1 = (Button) findViewById(R.id.btnUp);
+        Button b2 = (Button) findViewById(R.id.btnDown);
+        Button b3 = (Button) findViewById(R.id.btnLeft);
+        Button b4 = (Button) findViewById(R.id.btnRight);
+
+        ButtonControls bc = new ButtonControls(this);
+        b1.setOnTouchListener(bc);
+        b2.setOnTouchListener(bc);
+        b3.setOnTouchListener(bc);
+        b4.setOnTouchListener(bc);
+
+        final TextView tv = (TextView) findViewById(R.id.tvGameOver);
+        Thread timer = new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                int time = 0;
+                while (timerLoopAvailable) {
+                    try {
+                        sleep(1000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    time++;
+                    final int temp = time;
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            if(GameStatus.isEnd()) {
+                                tv.setText("GAME OVER :(");
+                                timerLoopAvailable=false;
+                            }
+                            else {
+
+                                tv.setText("Time: " + temp);
+                            }
+                        }
+                    });
+                }
+            }
+        };
+        timer.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        timerLoopAvailable=false;
+        super.onDestroy();
+    }
+}
