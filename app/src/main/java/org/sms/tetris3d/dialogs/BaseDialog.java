ee9d858d8d011fee11c1d859c9ddf8c51f9ebe47
hsh97Erica <imscs21@hanyang.ac.kr>
2016-11-20 11:13:18 +0900

    add dialog classes skel

---
+++ app/src/main/java/org/sms/tetris3d/dialogs/BaseDialog.java
@@ -0,0 +1,27 @@
+package org.sms.tetris3d.dialogs;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.*;
+/**
+ * Created by hsh on 2016. 11. 20..
+ */
+
+public class BaseDialog extends AlertDialog {
+    public BaseDialog(Context ctx){
+        super(ctx);
+
+    }
+    public BaseDialog(Context ctx,int themeResId){
+        super(ctx,themeResId);
+    }
+    public void setTopLayout(int resId){
+        setContentView(resId);
+    }
+    public void addListView(int resId){
+        ViewGroup layout = (ViewGroup)findViewById(resId);//top layout
+        layout.addView(new ListView(getContext()));
+    }
+
+}
---
+++ app/src/main/java/org/sms/tetris3d/dialogs/PauseDialog.java
@@ -0,0 +1,8 @@
+package org.sms.tetris3d.dialogs;
+
+/**
+ * Created by hsh on 2016. 11. 20..
+ */
+
+public class PauseDialog {
+}
